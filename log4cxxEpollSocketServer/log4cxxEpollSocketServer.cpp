dae943e8db7dea4b532f10228e029ce4b32db1f9
hyunsu00 <hyun955807@naver.com>
2021-06-14 23:17:18 +0900

    linux cmake 수정

--- log4cxxEpollSocketServer/log4cxxEpollSocketServer.cpp
+++ log4cxxEpollSocketServer/log4cxxEpollSocketServer.cpp
@@ -12,38 +12,38 @@
 #include <arpa/inet.h>
 
 #include "ObjectLoader.h"
-#include <string.h> // strdup
-#include <libgen.h> // dirname
+#include <string.h>						  // strdup
+#include <libgen.h>						  // dirname
 #include <log4cxx/propertyconfigurator.h> // log4cxx::PropertyConfigurator
-#include <log4cxx/helpers/exception.h> // log4cxx::helpers::InstantiationException, log4cxx::helpers::RuntimeException
+#include <log4cxx/helpers/exception.h>	  // log4cxx::helpers::InstantiationException, log4cxx::helpers::RuntimeException
 
-#define LISTEN_BACKLOG 15	// 일반적인 connection의 setup은 client가 connect()를 사용하여 connection request를 전송하면 server는 accept()를 사용하여 connection을 받아들입니다. 
-							// 그런데 만약 server가 다른 작업을 진행 중이면 accept()를 할 수 없는 경우가 발생하고 이 경우 connection request는 queue에서 대기하는데 backlog는 
-							// 이와 같이 accept() 없이 대기 가능한 connection request의 최대 개수입니다. 
-							// 보통 5정도의 value를 사용하며 만약 아주 큰 값을 사용하면 kernel의 resource를 소모합니다.
-							// 따라서, 접속 가능한 클라이언트의 최대수가 아닌 연결을 기다리는 클라이언트의 최대수입니다
+#define LISTEN_BACKLOG 15 // 일반적인 connection의 setup은 client가 connect()를 사용하여 connection request를 전송하면 server는 accept()를 사용하여 connection을 받아들입니다.        \
+						  // 그런데 만약 server가 다른 작업을 진행 중이면 accept()를 할 수 없는 경우가 발생하고 이 경우 connection request는 queue에서 대기하는데 backlog는 \
+						  // 이와 같이 accept() 없이 대기 가능한 connection request의 최대 개수입니다.                                                                                           \
+						  // 보통 5정도의 value를 사용하며 만약 아주 큰 값을 사용하면 kernel의 resource를 소모합니다.                                                                   \
+						  // 따라서, 접속 가능한 클라이언트의 최대수가 아닌 연결을 기다리는 클라이언트의 최대수입니다
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	char* exePath = strdup(argv[0]);
+	char *exePath = strdup(argv[0]);
 	std::string exeDir = dirname(exePath);
 	free(exePath);
 	exeDir += "/";
 	std::string filePath = exeDir + "log4cxxEpollSocketServer.conf";
 	log4cxx::PropertyConfigurator::configure(log4cxx::File(filePath));
 
-    printf("run log4cxxEpollSocketServer\n");
-	int	error_check;
+	printf("run log4cxxEpollSocketServer\n");
+	int error_check;
 
 	// 소켓 생성
-	int	server_fd = socket(PF_INET, SOCK_STREAM, 0);
+	int server_fd = socket(PF_INET, SOCK_STREAM, 0);
 	if (server_fd < 0) {
 		printf("socket() error\n");
 		return 0;
 	}
 
-	// server fd Non-Blocking Socket으로 설정. Edge Trigger 사용하기 위해 설정. 
-	int	flags = fcntl(server_fd, F_GETFL);
+	// server fd Non-Blocking Socket으로 설정. Edge Trigger 사용하기 위해 설정.
+	int flags = fcntl(server_fd, F_GETFL);
 	flags |= O_NONBLOCK;
 	if (fcntl(server_fd, F_SETFL, flags) < 0) {
 		printf("server_fd fcntl() error\n");
@@ -52,8 +52,8 @@ int main(int argc, char* argv[])
 
 	// 소켓 옵션 설정.
 	// Option -> SO_REUSEADDR : 비정상 종료시 해당 포트 재사용 가능하도록 설정
-	int	option = true;
-	error_check	= setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
+	int option = true;
+	error_check = setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
 	if (error_check < 0) {
 		printf("setsockopt() error[%d]\n", error_check);
 		close(server_fd);
@@ -68,7 +68,7 @@ int main(int argc, char* argv[])
 	mSockAddr.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY : 사용가능한 랜카드 IP 사용
 
 	// 소켓 속성과 소켓 fd 연결
-	error_check	= bind(server_fd, (struct sockaddr*)&mSockAddr, sizeof(mSockAddr));
+	error_check = bind(server_fd, (struct sockaddr *)&mSockAddr, sizeof(mSockAddr));
 	if (error_check < 0) {
 		printf("bind() error[%d]\n", error_check);
 		close(server_fd);
@@ -83,7 +83,7 @@ int main(int argc, char* argv[])
 	}
 
 	// Epoll fd 생성
-	int	epoll_fd = epoll_create(1024);	// size 만큼의 커널 폴링 공간을 만드는 함수
+	int epoll_fd = epoll_create(1024); // size 만큼의 커널 폴링 공간을 만드는 함수
 	if (epoll_fd < 0) {
 		printf("epoll_create() error\n");
 		close(server_fd);
@@ -99,9 +99,10 @@ int main(int argc, char* argv[])
 	// 보면 더이상의 상태 변화가 없기 때문이죠..LT 또는 ET는 쉽게 옵션으로 설정 가능합니다.
 	// 참고로 select / poll은 레벨트리거만 지원합니다.
 	struct epoll_event events;
-	events.events = EPOLLIN | EPOLLET;
+	// events.events = EPOLLIN | EPOLLET;
+	events.events = EPOLLIN;
 	events.data.fd = server_fd;
-	
+
 	/* server events set(read for accept) */
 	// epoll_ctl : epoll이 관심을 가져주기 바라는 FD와 그 FD에서 발생하는 event를 등록하는 인터페이스.
 	// EPOLL_CTL_ADD : 관심있는 파일디스크립트 추가
@@ -121,16 +122,21 @@ int main(int argc, char* argv[])
 	// timeout	-1 		--> 영원히 사건 기다림(blocking)
 	// 			0  		--> 사건 기다리지 않음.
 	//			0 < n 	--> (n)ms 만큼 대기
-	int	MAX_EVENTS = 1024;
+	int MAX_EVENTS = 1024;
 	struct epoll_event epoll_events[MAX_EVENTS];
-	int	event_count;
+	int event_count;
 	int timeout = -1;
 
+	int clientCount = 0;
 	while (true) {
-		event_count	= epoll_wait(epoll_fd, epoll_events, MAX_EVENTS, timeout);
-		printf("event count[%d]\n", event_count);
+		event_count = epoll_wait(epoll_fd, epoll_events, MAX_EVENTS, timeout);
+		// printf("event count[%d]\n", event_count);
 
 		if (event_count < 0) {
+			if (errno == EINTR) { // 신호처리기(gdb)에 의해 중단됨
+				continue;
+			}
+
 			printf("epoll_wait() error [%d]\n", event_count);
 			return 0;
 		}
@@ -138,16 +144,16 @@ int main(int argc, char* argv[])
 		for (int i = 0; i < event_count; i++) {
 			// Accept
 			if (epoll_events[i].data.fd == server_fd) {
-				int	client_fd;
-				int	client_len;
-				struct sockaddr_in	client_addr;
-				
-				printf("User Accept\n");
-				client_len = sizeof(client_addr);
-				client_fd = accept(server_fd, (struct sockaddr*)&client_addr, (socklen_t*)&client_len);
+				int client_fd;
+				int client_len;
+				struct sockaddr_in client_addr;
 
-				// client fd Non-Blocking Socket으로 설정. Edge Trigger 사용하기 위해 설정. 
-				int	flags = fcntl(client_fd, F_GETFL);
+				// printf("User Accept\n");
+				client_len = sizeof(client_addr);
+				client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_len);
+
+				// client fd Non-Blocking Socket으로 설정. Edge Trigger 사용하기 위해 설정.
+				int flags = fcntl(client_fd, F_GETFL);
 				flags |= O_NONBLOCK;
 				if (fcntl(client_fd, F_SETFL, flags) < 0) {
 					printf("client_fd[%d] fcntl() error\n", client_fd);
@@ -160,8 +166,9 @@ int main(int argc, char* argv[])
 				}
 
 				// 클라이언트 fd, epoll 에 등록
-				struct epoll_event	events;
-				events.events = EPOLLIN | EPOLLET;
+				struct epoll_event events;
+				// events.events = EPOLLIN | EPOLLET;
+				events.events = EPOLLIN;
 				events.data.fd = client_fd;
 
 				if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &events) < 0) {
@@ -169,21 +176,23 @@ int main(int argc, char* argv[])
 					close(client_fd);
 					continue;
 				}
+
+				printf("Client Accept client_fd[%d], clientCount[%d]\n", client_fd, ++clientCount);
 			} else {
 				// epoll에 등록 된 클라이언트들의 send data 처리
-				int	client_fd = epoll_events[i].data.fd;
+				int client_fd = epoll_events[i].data.fd;
 				const size_t BUF_LEN = 4096;
 				std::vector<char> readBuf(BUF_LEN, 0);
 				int readBytes = read(client_fd, &readBuf[0], readBuf.size());
 
 				if (readBytes == 0) {
 					// 클라이언트 접속 종료 요청
-					printf("Client Disconnect [%d]\n", client_fd);
+					printf("Client Disconnect client_fd[%d], clientCount[%d]\n", client_fd, --clientCount);
 					close(client_fd);
 					epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL);
 				} else {
 					// 접속 종료 요청이 아닌 경우 요청의 내용에 따라 처리.
-					printf("Recv Data from [%d] : readBytes = %d\n", client_fd, readBytes);
+					// printf("Recv Data from [%d] : readBytes = %d\n", client_fd, readBytes);
 				}
 			}
 		}
